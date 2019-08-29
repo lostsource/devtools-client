85152b9e46b4e3e5e8bae1d5358b7bf01bc9fdef
Joseph Portelli <joseph@lostsource.com>
2019-08-29 23:33:34 +0200

    initial commit

---
+++ example/heapusage.js
@@ -0,0 +1,16 @@
+const V8Control = require('../lib/V8Control').control;
+const control = new V8Control(); 
+
+control.connect({
+  // replace with output of `node --inspect yourapp.js` 
+  nodeWSEndpoint: 'ws://127.0.0.1:9229/66cafdb2-9e86-45a6-8079-0b765d585e31' 
+}).then(async ({Debugger, Runtime, Profiler}) => {  
+
+  await Runtime.enable();
+  Runtime.getHeapUsage().then((result) => {
+    console.log(result);
+  });
+  
+}).catch((e) => {
+  console.log(e);
+});
